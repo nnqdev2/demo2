9eb59b8bb87335843b1a84f1839c8374e3f45b2a
QUAN Nga <nquan@deq.state.or.us>
2018-04-11 07:33:01 -0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { IncidentComponent } from './incident/incident.component';
+import { IncidentService } from './incident.service';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncidentComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(), ReactiveFormsModule, HttpClientModule, BrowserModule
   ],
-  providers: [],
+  providers: [IncidentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
